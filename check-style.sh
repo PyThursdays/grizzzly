f6765e538de09ac5ecc757429581a1276f5a0d50
Alejandro Rivas <alexrivas502@gmail.com>
2022-03-25 00:35:33 -0600

    GZ-2 Created src folder

--- check-style.sh
+++ check-style.sh
@@ -1,3 +1,2 @@
 pycodestyle setup.py --max-line-length 120
-pycodestyle src/numba_namespace_extension/ --max-line-length 120
-pycodestyle example/ --max-line-length 120
+pycodestyle src/grizzzley/ --max-line-length 120
