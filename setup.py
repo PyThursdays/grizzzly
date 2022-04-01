2f2d4c9eddb83ef294b4caf5a8b94b2e055e2aa4
root <root@SLASH.localdomain>
2022-03-31 23:12:10 -0600

    GZ-16 Add Install_requires to setup

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ setup(
     name="grizzzly",
     version="0.1.0",
     description="This is your favorite data sharing library! Works great with pandas.",
+    install_requires=[
+        'fire==0.4.0'
+    ],
     package_dir={
         "": "src"
     },
