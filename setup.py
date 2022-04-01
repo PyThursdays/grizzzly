cd3f93c3cbc0049005150d6ef8e78455acdb39ce
AlexRivas502 <alexrivas502@gmail.com>
2022-03-31 23:30:19 -0600

    GZ-16 Added requirements into setup

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from setuptools import setup, find_namespace_packages
 with open("README.md", "r") as file:
     readme = file.read()
 
+with open("requirements.txt", "r") as file:
+    install_requirements = file.read().splitlines()
+
 
 setup(
     name="grizzzly",
@@ -13,7 +16,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_namespace_packages(where="src"),
     install_requires=[
-        'fire==0.4.0'
+        install_requirements
     ],
     package_dir={
         "": "src"
