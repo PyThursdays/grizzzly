ea586cc55695b4e27d0fa9d71bebd9be6a7f4a56
rhdzmota <rhdzmota@gmail.com>
2022-04-08 00:23:37 -0500

    GZ-33 Update CLI with backend

--- src/grizzzly/cli.py
+++ src/grizzzly/cli.py
@@ -1,11 +1,54 @@
 import time
+import logging
 from typing import Optional
+
+
+from .settings import (
+    GZ_FLASK_HOST,
+    GZ_FLASK_PORT,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class CLIServer:
+
+    def run(
+            self,
+            host: Optional[str] = None,
+            port: Optional[str] = None,
+            debug: bool = False,
+            prod: bool = False,
+    ):
+        import importlib
+
+        # Server configuration
+        host = host or GZ_FLASK_HOST
+        port = port or GZ_FLASK_PORT
+        # Get server flask application
+        server = importlib.import_module("grizzzly.server")
+        app = getattr(server, "app")
+        # Start the server
+        if not prod:
+            return app.run(
+                host=host,
+                port=port,
+                debug=debug,
+            )
+        waitress = importlib.import_module("waitress")
+        serve = getattr(waitress, "serve")
+        return serve(
+            app,
+            host=host,
+            port=port,
+        )
 
 
 class CLI:
 
     def __init__(self):
         self.start = time.time()
+        self.backend = CLIServer()
 
     def hello(self, name: Optional[str] = None):
         name = name or "world"
