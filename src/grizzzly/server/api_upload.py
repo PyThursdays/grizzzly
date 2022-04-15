739a0e21bc2df2b13047fe97d3c6392a83541b37
rhdzmota <rhdzmota@gmail.com>
2022-04-15 02:01:45 -0500

    GZ-38 Add a simple create endpoint

--- src/grizzzly/server/api_upload.py
+++ src/grizzzly/server/api_upload.py
@@ -1,4 +1,11 @@
+import os
+
 from flask import Blueprint, request
+
+from grizzzly.settings import GZ_BASEPATH, get_logger
+
+
+logger = get_logger(__name__)
 
 
 # Create an upload endpoint
@@ -7,6 +14,28 @@ api_upload = Blueprint(
     import_name=__name__,
     url_prefix="/upload"
 )
+
+
+@api_upload.route("/create", methods=["GET"])
+def create():
+    # Be sure to create the filesystem configurations
+    # TODO: Allow overwriting
+    params = request.args.to_dict()
+    name = params.get("name")
+    author = params.get("author", "generic")  # Generic public author
+    if not name or not author:
+        return "Missing name parameter", 400
+    relative_output_path = os.path.join(author, name)
+    absolute_output_path = os.path.join(
+        GZ_BASEPATH,
+        relative_output_path
+    )
+
+    logger.info("Creating the absolute output path")
+    os.makedirs(absolute_output_path, exist_ok=True)
+    return "ok!"
+
+
 
 
 @api_upload.route("/", methods=["GET", "POST"]) # Remove GET when we stop browser testing
