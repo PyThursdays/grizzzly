7a1ca7645ec1c3217321733b77a5e2158339cf5d
rhdzmota <rhdzmota@gmail.com>
2022-04-15 00:37:13 -0500

    GZ-39 Retrive default dataset when name is not provided

--- src/grizzzly/client/download.py
+++ src/grizzzly/client/download.py
@@ -1,16 +1,22 @@
+from typing import Optional
+
 import requests
 import pandas as pd
 
 from grizzzly.settings import (
+    get_logger,
     GZ_ENDPOINT_ALIAS,
     GZ_FLASK_HOST,
     GZ_FLASK_PORT,
 )
 
 
+logger = get_logger(__name__)
 
-def download_dataset(name: str) -> pd.DataFrame:
-    url = f"http://{GZ_FLASK_HOST}:{GZ_FLASK_PORT}/download"
+
+def download_dataset(name: Optional[str] = None) -> pd.DataFrame:
+    if not name:
+        logger.warning("Name was not provided; retrieving the default dataset")
     response = requests.get(GZ_ENDPOINT_ALIAS["download-dataset"])
     if not response.ok:
         raise ValueError("There was a problem while getting the dataset")
